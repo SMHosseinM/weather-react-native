29999c789c1c70e3ad0c99e8f0f355a4869760b2
hossein <smh.miraftabi1373@gmail.com>
2022-09-03 09:37:43 +0100

    add built-in compnents and customise ones

--- weather/App.js
+++ weather/App.js
@@ -1,12 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Platform, KeyboardAvoidingView, ImageBackground, View } from 'react-native';
+import SearchInput from './components/SearchInput';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <KeyboardAvoidingView style={styles.container} behavior="padding">
+      <ImageBackground
+        source={require('./assets/background.png')}
+        style={styles.imageContainer}
+        imageStyle={styles.image}
+      >
+        <View>
+          <Text style={[styles.largeText, styles.textStyle]}>San Francisco</Text>
+          <Text style={[styles.smallText, styles.textStyle]}>Light Cloud</Text>
+          <Text style={[styles.largeText, styles.textStyle]}>24°</Text>
+          <SearchInput placeholder="Search any city" />
+          <StatusBar style="auto" />
+        </View>
+      </ImageBackground>
+    </KeyboardAvoidingView>
   );
 }
 
@@ -14,7 +26,24 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    // alignItems: 'center',
     justifyContent: 'center',
   },
+  textStyle: {
+    textAlign: 'center',
+    fontFamily: Platform.OS === 'ios' ? 'AvenirNext-Regular' : 'Roboto',
+  },
+  largeText: {
+    fontSize: 44,
+    color: "white"
+  },
+  smallText: {
+    fontSize: 18,
+    color: "white"
+  },
+  imageContainer: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center"
+  }
 });
